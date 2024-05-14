77f7bf18bed4ace10b5b86232980fcb668eff3bf
Nhat_Minh <nhatminhc14@gmail.com>
2024-05-14 22:51:26 +0700

    feat: Improve UI for chat

--- lib/pages/sub-pages/message_conversation.dart
+++ lib/pages/sub-pages/message_conversation.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:final_project_mobile/features/message/bloc/message_bloc.dart';
 import 'package:final_project_mobile/features/message/bloc/message_event.dart';
 import 'package:final_project_mobile/features/message/bloc/message_state.dart';
@@ -27,6 +25,7 @@ class _MessagesDetailsState extends State<MessagesDetails> {
   final TextEditingController _messageController = TextEditingController();
   late List<Conversation> conversations = [];
   late final UserProfile sender;
+  late String receiverName = '';
   @override
   void initState() {
     super.initState();
@@ -45,12 +44,19 @@ class _MessagesDetailsState extends State<MessagesDetails> {
       listener: (context, state) {
         if (state is AllMessagesInConversationLoadSuccess) {
           conversations = state.conversations;
+          if (conversations.isNotEmpty) {
+            receiverName = conversations[0].receiver.id != sender.id
+                ? conversations[0].receiver.fullname
+                : conversations[0].sender.fullname;
+          } else {
+            receiverName = 'Chat';
+          }
         }
       },
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
-            title: const Text('Chat'),
+            title: Text(receiverName ?? 'Chat'),
           ),
           body: Padding(
             padding: const EdgeInsets.all(20),
@@ -69,44 +75,74 @@ class _MessagesDetailsState extends State<MessagesDetails> {
 
                       bool isCurrentUser = conversation.sender.id == sender.id;
 
-                      return Align(
-                        alignment: isCurrentUser
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          margin: const EdgeInsets.symmetric(vertical: 4),
-                          decoration: BoxDecoration(
-                            color: isCurrentUser
-                                ? Colors.blue[100]
-                                : Colors.grey[300],
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                isCurrentUser ? 'You' : senderName,
-                                style: TextStyle(
-                                  fontWeight: isCurrentUser
-                                      ? FontWeight.bold
-                                      : FontWeight.normal,
-                                  color: isCurrentUser
-                                      ? Colors.blue
-                                      : Colors.black,
+                      return Container(
+                        margin: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            if (!isCurrentUser) // Display sender's avatar for other senders
+                              Container(
+                                margin: const EdgeInsets.only(right: 8),
+                                child: CircleAvatar(
+                                  // You can set the avatar image here
+                                  radius: 20,
+                                  backgroundColor: Colors.grey,
+                                  child: Text(
+                                    senderName.isNotEmpty
+                                        ? senderName
+                                            .split(' ')
+                                            .map((word) => word[0])
+                                            .join()
+                                        : '',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
                                 ),
                               ),
-                              const SizedBox(height: 4),
-                              Text(
-                                conversation.content ?? '',
+                            Expanded(
+                              child: Align(
+                                alignment: isCurrentUser
+                                    ? Alignment.centerRight
+                                    : Alignment.centerLeft,
+                                child: Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color: isCurrentUser
+                                        ? Colors.blue[100]
+                                        : Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      if (!isCurrentUser) // Display sender's name for other senders
+                                        Text(
+                                          senderName,
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      const SizedBox(height: 4),
+                                      Text(
+                                        conversation.content ?? '',
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 4),
+                                      Text(
+                                        formattedDate,
+                                        style: const TextStyle(fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
-                              const SizedBox(height: 4),
-                              Text(
-                                formattedDate,
-                                style: const TextStyle(fontSize: 12),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       );
                     },
